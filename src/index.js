f13407c93176fe9def45319fecfaa7dd3f22cea1
59naga <i59naga@icloud.com>
2015-11-24 01:28:56 +0900

    0.0.4 Fix #2
    * Change code-style 59naga to airbnb/base

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 // Dependencies
-import {EventEmitter} from 'events'
-import Promise from 'bluebird'
+import {EventEmitter} from 'events';
+import Promise from 'bluebird';
 
 // Public
-class Carrack extends EventEmitter{
-  once(event,listener){
-    if(typeof listener!=='function'){
-      throw new TypeError('listener must be a function')
+class Carrack extends EventEmitter {
+  once(event, listener) {
+    if (typeof listener !== 'function') {
+      throw new TypeError('listener must be a function');
     }
 
-    let fired= false
+    let fired = false;
 
-    let onceListener= (...args)=>{
-      this.removeListener(event,onceListener)
+    const onceListener = (...args)=>{
+      this.removeListener(event, onceListener);
 
-      if(fired===false){
-        fired= true
-        return listener(...args)
+      if (fired === false) {
+        fired = true;
+        return listener(...args);
       }
-    }
-    this.on(event,onceListener)
+    };
+    this.on(event, onceListener);
 
-    return this
+    return this;
   }
-  emit(event,...args){
-    let promises= []
+  emit(event, ...args) {
+    const promises = [];
 
     this.listeners(event).forEach(listener=>{
-      promises.push(listener(...args))
-    })
+      promises.push(listener(...args));
+    });
 
-    return Promise.all(promises)
+    return Promise.all(promises);
   }
 }
 
-export default Carrack
+export default Carrack;
--- test/index.js
+++ test/index.js
@@ -1,58 +1,58 @@
 // Dependencies
-import AsyncEmitter from '../src'
+import AsyncEmitter from '../src';
 
-import Promise from 'bluebird'
-import assert from 'power-assert'
+import Promise from 'bluebird';
+import assert from 'power-assert';
 
 // Specs
-describe('carrack',function(){
-  this.timeout(500)
+describe('carrack', function() {
+  this.timeout(500);
 
-  describe('.emit',()=>{
-    it('values is passed as an array of values from all the listeners',()=>{
-      let emitter
-      emitter= new AsyncEmitter
-      emitter.once('foo',(data)=>{
-        return new Promise((resolve,reject)=>{
-          resolve(data)
-        })
-      })
+  describe('.emit', ()=>{
+    it('values is passed as an array of values from all the listeners', ()=>{
+      let emitter;
+      emitter = new AsyncEmitter;
+      emitter.once('foo', (data)=>{
+        return new Promise((resolve, reject)=>{
+          resolve(data);
+        });
+      });
 
-      let promise
-      promise= emitter.emit('foo','bar')
+      let promise;
+      promise = emitter.emit('foo', 'bar')
       .then(values=>{
-        assert(values[0] === 'bar')
+        assert(values[0] === 'bar');
 
-        return emitter.emit('foo','bar')
+        return emitter.emit('foo', 'bar');
       })
       .then(values=>{
-        assert(values[0] === undefined)
-      })
+        assert(values[0] === undefined);
+      });
 
-      return promise
-    })
+      return promise;
+    });
 
-    it('discarding all the other promises whether or not they have resolved',()=>{
-      let emitter
-      emitter= new AsyncEmitter
-      emitter.on('foo',(data)=>{
-        return new Promise((resolve,reject)=>{
-          resolve(data)
-        })
-      })
-      emitter.on('foo',(data)=>{
-        return new Promise((resolve,reject)=>{
-          reject(new Error(data))
-        })
-      })
+    it('discarding all the other promises whether or not they have resolved', ()=>{
+      let emitter;
+      emitter = new AsyncEmitter;
+      emitter.on('foo', (data)=>{
+        return new Promise((resolve, reject)=>{
+          resolve(data);
+        });
+      });
+      emitter.on('foo', (data)=>{
+        return new Promise((resolve, reject)=>{
+          reject(new Error(data));
+        });
+      });
 
-      let promise
-      promise= emitter.emit('foo','bar').catch(reason=>{
-        assert(reason instanceof Error)
-        assert(reason.message === 'bar')
-      })
+      let promise;
+      promise = emitter.emit('foo', 'bar').catch(reason=>{
+        assert(reason instanceof Error);
+        assert(reason.message === 'bar');
+      });
 
-      return promise
-    })
-  })
-})
+      return promise;
+    });
+  });
+});
