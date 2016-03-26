62a6e97d238825d8f9c47b91e28355fd9ea220df
59naga <i59naga@icloud.com>
2016-03-27 04:04:09 +0900

    feat(subscribe): add `subscribe` method
    - add jsdoc for all methods
    - code reactor
    - rewrite spec:
      .emit
        should receive the return value of listeners asynchronously
        if an exception occurs, it should throw an exception only the first one
        if the promise was rejected, it should throw only the first of the reject
      .once
        listeners that have been executed should be removed immediately
        if the argument isn't function, should throw an exception
      .subscribe
        if executed the return value, should remove the listener
        if specify third argument is true, should remove the listener after executed
      issues
        .once
          #3: always the listener should be stopped at the removeListener

--- src/index.js
+++ src/index.js
@@ -4,13 +4,37 @@ import Promise from 'bluebird';
 
 // @class AsyncEmitter
 export default class AsyncEmitter extends EventEmitter {
+  /**
+  * run the listener to expect a promise
+  *
+  * @param {string} event - a event name
+  * @param {any} arguments - a arguments pass to listeners
+  * @returns {promise<any>} - the return value of listeners
+  */
+  emit(event, ...args) {
+    const promises = [];
+
+    this.listeners(event).forEach(listener => {
+      promises.push(listener(...args));
+    });
+
+    return Promise.all(promises);
+  }
+
+  /**
+  * emits a 'removeListener' event iff the listener was removed
+  * (redefine for inherited method doesn't work)
+  *
+  * @param {string} event - a event name
+  * @param {function} listener - a listener function
+  * @returns {asyncEmitter} this
+  */
   once(event, listener) {
     if (typeof listener !== 'function') {
       throw new TypeError('listener must be a function');
     }
 
     let fired = false;
-
     const onceListener = (...args) => {
       this.removeListener(event, onceListener);
 
@@ -27,13 +51,26 @@ export default class AsyncEmitter extends EventEmitter {
 
     return this;
   }
-  emit(event, ...args) {
-    const promises = [];
 
-    this.listeners(event).forEach(listener => {
-      promises.push(listener(...args));
-    });
+  /**
+  * register an event listener, returns the remove function
+  *
+  * @param {string} event - a event name
+  * @param {function} listener - a event name
+  * @param {boolean} once=false - if true, listener is call only once
+  * @returns {function} unsubscribe - the remove function of listener
+  */
+  subscribe(event, listener, once = false) {
+    const unsubscribe = () => {
+      this.removeListener(event, listener);
+    };
 
-    return Promise.all(promises);
+    if (once) {
+      this.once(event, listener);
+    } else {
+      this.on(event, listener);
+    }
+
+    return unsubscribe;
   }
 }
--- test/index.js
+++ test/index.js
@@ -1,64 +1,136 @@
 // dependencies
-import AsyncEmitter from '../src';
-
 import Promise from 'bluebird';
 import assert from 'power-assert';
+import assertRejected from 'assert-rejected';
+
+// target
+import AsyncEmitter from '../src';
 
 // specs
-describe('carrack', function () {
-  this.timeout(500);
-
+describe('carrack', () => {
   describe('.emit', () => {
-    it('values is passed as an array of values from all the listeners', () => {
+    it('should receive the return value of listeners asynchronously', () => {
       const emitter = new AsyncEmitter;
-      emitter.once('foo', (data) => {
-        return new Promise((resolve) => {
-          resolve(data);
-        });
-      });
+      emitter.on('foo', (action) => action);
+      emitter.on('foo', (action) => new Promise((resolve) => {
+        setTimeout(() => resolve(action));
+      }));
 
-      const promise = emitter.emit('foo', 'bar')
-      .then(values => {
+      return emitter.emit('foo', 'bar').then((values) => {
+        assert(values.length === 2);
+        assert(values[0] === 'bar');
+        assert(values[1] === 'bar');
+      });
+    });
+
+    it('if an exception occurs, it should throw an exception only the first one', () => {
+      const emitter = new AsyncEmitter;
+      emitter.on('foo', (action) => action);
+      emitter.on('foo', () => new Promise(() => {
+        throw new Error('kaboom');
+      }));
+      emitter.on('foo', () => Promise.reject(new Error('kabo?')));
+
+      return assertRejected(emitter.emit('foo', 'bar'), 'kaboom');
+    });
+
+    it('if the promise was rejected, it should throw only the first of the reject', () => {
+      const emitter = new AsyncEmitter;
+      emitter.on('foo', (action) => action);
+      emitter.on('foo', () => Promise.reject(new Error('kabo?')));
+      emitter.on('foo', () => new Promise(() => {
+        throw new Error('kaboom');
+      }));
+
+      return assertRejected(emitter.emit('foo', 'bar'), 'kabo?');
+    });
+  });
+
+  describe('.once', () => {
+    it('listeners that have been executed should be removed immediately', () => {
+      const emitter = new AsyncEmitter;
+      emitter.once('foo', (action) => action);
+
+      return emitter.emit('foo', 'bar')
+      .then((values) => {
+        assert(values.length === 1);
         assert(values[0] === 'bar');
 
         return emitter.emit('foo', 'bar');
       })
-      .then(values => {
+      .then((values) => {
+        assert(values.length === 0);
         assert(values[0] === undefined);
       });
-
-      return promise;
     });
 
-    it('discarding all the other promises whether or not they have resolved', () => {
+    it("if the argument isn't function, should throw an exception", () => {
       const emitter = new AsyncEmitter;
-      emitter.on('foo', (data) => {
-        return new Promise((resolve) => {
-          resolve(data);
-        });
-      });
-      emitter.on('foo', (data) => {
-        return new Promise((resolve, reject) => {
-          reject(new Error(data));
-        });
-      });
-
-      const promise = emitter.emit('foo', 'bar').catch(reason => {
-        assert(reason instanceof Error);
-        assert(reason.message === 'bar');
-      });
-
-      return promise;
+      assert.throws(
+        () => {
+          emitter.once('foo', 'bad argument!');
+        },
+        (error) => {
+          assert(error.message === 'listener must be a function');
+          return true;
+        }
+      );
     });
+  });
 
-    it('always the listener should be stopped at the removeListener', () => {
-      const listener = () => 1;
+  describe('.subscribe', () => {
+    it('if executed the return value, should remove the listener', () => {
       const emitter = new AsyncEmitter;
-      emitter.once('foo', listener);
-      emitter.removeListener('foo', listener);
+      const unsubcribe = emitter.subscribe('foo', (action) => action);
 
-      return emitter.emit('foo').then((values) => {
+      return emitter.emit('foo', 'bar')
+      .then((values) => {
+        assert(values.length === 1);
+        assert(values[0] === 'bar');
+
+        unsubcribe();
+
+        return emitter.emit('foo', 'bar');
+      })
+      .then((values) => {
+        assert(values.length === 0);
         assert(values[0] === undefined);
+      });
+    });
+
+    it('if specify third argument is true, should remove the listener after executed', () => {
+      const emitter = new AsyncEmitter;
+
+      const unsubscribe = emitter.subscribe('foo', (action) => action, true);
+      unsubscribe();
+
+      emitter.subscribe('foo', (action) => action, true);
+      return emitter.emit('foo', 'bar')
+      .then((values) => {
+        assert(values.length === 1);
+        assert(values[0] === 'bar');
+
+        return emitter.emit('foo', 'bar');
+      })
+      .then((values) => {
+        assert(values.length === 0);
+        assert(values[0] === undefined);
+      });
+    });
+  });
+
+  describe('issues', () => {
+    describe('.once', () => {
+      it('#3: always the listener should be stopped at the removeListener', () => {
+        const listener = () => 1;
+        const emitter = new AsyncEmitter;
+        emitter.once('foo', listener);
+        emitter.removeListener('foo', listener);
+
+        return emitter.emit('foo').then((values) => {
+          assert(values.length === 0);
+          assert(values[0] === undefined);
+        });
       });
     });
   });
